9307f5e06c29feb885a4a1d55f4588e079b9d9f6
DJ <d.j_m@hotmail.com>
2026-03-03 23:32:17 -0500

    Added backend, moved docs into docs folder, wired in storage for access to blob + DB.

---
+++ src/test/java/ca/group13/codecomparator/CodeCompareAppTests.java
@@ -0,0 +1,12 @@
+package ca.group13.codecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CodeCompareApp.class)
+class CodeCompareAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
