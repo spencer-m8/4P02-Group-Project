ee1546fb3c8dfa78fe5bb41800ab6c156c9ff7ba
Shivam Soul <99904523+Shivzam@users.noreply.github.com>
2026-03-13 22:19:51 -0400

    added comments

--- src/main/java/Engine/Engine.java
+++ src/main/java/Engine/Engine.java
@@ -6,26 +6,24 @@ import java.util.List;
 
 import static Engine.FingerPrintGenerator.*;
 
-
-
 public class Engine {
 
-    private final int k;
-    private final int w;
-
+    private final int k;   //k size in k-gram, k tokens ro form k-gram to be hashed
+    private final int w;   //w is used for window size in the winnowing process to select fingerprints
 
     public Engine(int k, int t) {
         this.k = k;
-        if (k > t) {
+        
+        if (k > t) {   // Winnowing requires t to greater than or equal to k
             throw new IllegalArgumentException("k cannot be greater than t");
         }
         this.w = t - k + 1;
     }
 
+    //runs the FingerPrintGenerator for all zip submissions
     public Submission[] run(List<Path> zipPaths) throws IOException {
         return FingerPrintGenerator.generateFingerprints(zipPaths, k, w);
     }
-
 
 
     //will update this once we integrate cloud storage, such that it will check to see if a zip has already been analyzed
