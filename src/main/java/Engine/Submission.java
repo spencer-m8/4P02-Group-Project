1abe9488e723c0c4b64b1cda78f9da5efc3f081b
Shivam Soul <99904523+Shivzam@users.noreply.github.com>
2026-03-13 23:37:23 -0400

    added comments

--- src/main/java/Engine/Submission.java
+++ src/main/java/Engine/Submission.java
@@ -3,14 +3,13 @@ package Engine;
 
 import java.util.List;
 
-public class Submission {
+public class Submission { 
 
-    private final String ID;
-    private final String language;
+    private final String ID; //Unique submission identifier 
+    private final String language; // identifies of coding language between (java, c and cpp)
 
-    private List<int[]> fingerprint;
-    private int fingerPrintSize;
-
+    private List<int[]> fingerprint; // list of fingerprint hashes, chosen by winnowing algorithm
+    private int fingerPrintSize; // number of fingerprints
 
     public Submission(String ID, String language, List<int[]> fingerprint) {
         this.ID = ID;
@@ -18,11 +17,19 @@ public class Submission {
         this.fingerprint = fingerprint;
     }
 
-
+    //Returns Submission ID
     public String getID() { return ID; }
+    
+    //Returns the language
     public String getLanguage() { return language; }
-
-    public void addFingerprint(List<int[]> fingerprint){this.fingerprint = fingerprint;this.fingerPrintSize = fingerprint.size();}
+    
+    //adds new fingerprint to list and increase size
+    public void addFingerprint(List<int[]> fingerprint){
+        this.fingerprint = fingerprint;
+        this.fingerPrintSize = fingerprint.size();
+    }
+    //returns list of fingerprints
     public List<int[]> getFingerprint(){return  fingerprint;}
+    //returns the number of fingerprints
     public int getFingerPrintSize(){return fingerPrintSize;}
 }
